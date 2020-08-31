66625e57921110693f5f764e3330b3e108a68f93
satan <rotterdam.xxi.century@gmail.com>
2020-08-31 16:24:19 +0600

    init

---
+++ Assets/Scripts/CameraRotator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraRotator : MonoBehaviour
+{
+    public float speed = 5f;
+
+    private Transform _transform;
+
+    private void Start()
+    {
+        _transform = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        _transform.Rotate(0, speed * Time.deltaTime, 0);
+    }
+}
---
+++ Assets/Scripts/CubeExploder.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CubeExploder : MonoBehaviour
+{
+    private bool _collisionSet;
+    private void OnCollisionEnter(Collision other)
+    {
+        if (!other.gameObject.CompareTag("Cube") && _collisionSet) return;
+        
+        for (var i = other.transform.childCount - 1; i >= 0; i--)
+        {
+            var child = other.transform.GetChild(i);
+            child.gameObject.AddComponent<Rigidbody>();
+            child.gameObject.GetComponent<Rigidbody>().AddExplosionForce(70f, Vector3.up, 5f);
+            child.SetParent(null);
+        }
+        
+        Destroy(other.gameObject);
+        _collisionSet = true;
+    }
+}
